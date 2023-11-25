5531e164969a935aa178e0698b0ce457a3c389aa
Md.Samsul Arefin <samsularefin532@gmail.com>
2023-11-25 23:59:33 +0600

    infinite scroll done

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 app.use(cors())
 app.use(express.json())
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wtx9jbs.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,22 +31,52 @@ Dbconnect();
 
 // collections
 const allMeals = client.db('FodeciousDb').collection('Allmeals');
+const allReviews = client.db('FodeciousDb').collection('Allreviews')
 
 
 app.get('/', async (req, res) => {
     res.send('Fodecious server is running');
 })
 
-app.get('/meals', async (req, res) => {
-    try {
-        const result = await allMeals.find().toArray();
-        res.send(result)
-
-
-    } catch (error) {
-        console.log(error.message);
+app.get('/allreviews', async (req, res) => {
+    const title = req.query.title;
+    let review_filter = {}
+    if (title) {
+        review_filter = { title: title }
     }
+    try {
+        const result = await allReviews.find(review_filter).toArray();
+        res.send(result)
+    } catch (error) {
+        console.log(error);
+    }
+
+
 })
+
+
+app.get('/meals', async (req, res) => {
+    const { limit, offset } = req.query;
+    console.log(limit, offset);
+    const meals = await allMeals.find().skip(Number(offset)).limit(Number(limit)).toArray();
+    res.send(meals);
+});
+
+
+// app.get('/meals', async (req, res) => {
+//     const id = req.query.id;
+//     console.log(id, "id on server");
+//     let query = {}
+//     if (id) {
+//         query._id = new ObjectId(id)
+//     }
+//     try {
+//         const result = await allMeals.find(query).toArray();
+//         res.send(result)
+//     } catch (error) {
+//         console.log(error.message);
+//     }
+// })
 
 
 
